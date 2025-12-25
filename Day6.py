3cf8f91c9f58d7b9350b18ca6008332fbb5226db
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2025-12-25 12:19:31 +0530

    update

--- Day6.py
+++ Day6.py
@@ -1,3 +1,16 @@
 #Python Operators
 #Operators are symbols that tell Python to perform an action on values or variables.
 print(10+5)
+print(100+12)
+
+#Python divides the operators in the following groups:
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+
