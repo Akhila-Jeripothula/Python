72fe72323cdae3aba8a450ca84a5969134ea2145
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2025-12-28 18:33:54 +0530

    update

--- Day9.py
+++ Day9.py
@@ -2,15 +2,35 @@
 # Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name.
 # Without list comprehension you will have to write a for statement with a conditional test inside:
 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]#for loop
 newlist = []
 for x in fruits:
   if "a" in x:
     newlist.append(x)
 print(newlist)
 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]#list comprehension
 newlist = [x for x in fruits if "a" in x]
 print(newlist)
 
 
+
+#Sorting the list
+x=["apple","pineapple","kiwi","orange","custardapple","mango"]   #sort alphabetically
+y=[23,32,12,15,2,48,31]                                          #sort numerically
+x.sort()
+y.sort()
+print(x)
+print(y)
+x.sort(reverse=True)       #sort in Descending order
+print(x)
+x.reverse()                #sort in reverse order
+print(x)
+
+
+
+#copy list
+# Make a copy of a list with the copy() method:
+a=["rose","tulip"]
+mylist=a.copy()
+print(mylist)
