a4c70253a660fa3f8d1429c05fbf8421835082ab
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2025-12-31 17:49:05 +0530

    day10

--- Day10.py
+++ Day10.py
@@ -36,3 +36,32 @@ y.append("orange")
 x = tuple(y)
 print(x)
 
+#unpacking
+# When we create a tuple, we normally assign values to it. This is called "packing" a tuple:
+fruits = ("apple", "banana", "cherry")
+# But, in Python, we are also allowed to extract the values back into variables. This is called "unpacking":
+fruits = ("apple", "banana", "cherry")
+(green, yellow, red) = fruits
+print(green)
+print(yellow)
+print(red)
+
+#Asterisk-If the number of variables is less than the number of values, you can add an * to the variable name and the values will be assigned to the variable as a list:
+
+#Loop Tuples
+
+# You can loop through the tuple items by using a for loop.
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+#Loop through index number-(range,len)
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+#while loop-Use the len() function to determine the length of the tuple, then start at 0 and loop your way through the tuple items by referring to their indexes.
+# Remember to increase the index by 1 after each iteration.
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
