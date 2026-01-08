81c5f5c14c2ca6160495aea738f171ca35122996
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-01-08 18:44:06 +0530

    day16

---
+++ Day16.py
@@ -0,0 +1,52 @@
+# While Loop
+#while loop is used to repeat the code till the condition is true
+
+#syntax: while(condition):
+           
+# 1                              
+i=3                                  
+while(i<=9):                        
+    print(i)
+    i+=1
+
+    # i=3
+    # while(i<=9):
+    #     print(i)  error because it is infinite loop
+
+
+
+# 2. while loop with user input
+# password=""
+
+# while password!= "python":
+#     password=input("Enter password")
+# print("Access granted")
+
+
+# 3. while loop using break statement
+i = 1
+while i <= 10:
+    if i == 5:
+        break
+    print(i)
+    i += 1
+
+
+# 4. while loop with continue statement(skips the current loop)
+i=1
+while(i<=10):
+    i+=1
+    if i==5:
+        continue
+    print(i)
+
+
+# 5. while loop with else(only when loop ends normally)
+
+
+
+
+
+
+
+
