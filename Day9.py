a09c67f9db76ae45669a8c16e09013e24a0523db
Akhila Jeripothula <akhilajeripothula075@gmail.com>
2026-01-08 19:15:24 +0530

    Update Day9.py

--- Day9.py
+++ Day9.py
@@ -2,7 +2,7 @@
 # Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name.
 # Without list comprehension you will have to write a for statement with a conditional test inside:
 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]#for loop
+fruits = ["apple", "banana", "cherries", "kiwi", "mango"]#for loop
 newlist = []
 for x in fruits:
   if "a" in x:
