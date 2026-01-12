1308b78782762164c7fc291a2cd4d0bcbe89d96c
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-01-12 19:32:51 +0530

    pattern

--- Day17.py
+++ Day17.py
@@ -41,22 +41,41 @@ i=1
 while(i<=5):
     print("*"*5)
     i+=1
-
+#output:
 # *****
 # *****
 # *****
 # *****
 # *****
 
-
+#4.
 i=1
 while(i<=5):
     print(i*i)
     i+=1
 
-
+#output:
 # 1
 # 4
 # 9
 # 16
 # 25
+
+
+#5.sum of numbers
+sum = 0
+for i in range(1, 11):
+    sum += i
+print(sum)
+#output-55
+
+#6.Largest number
+nums = [10, 45, 2, 90, 33]
+largest = nums[0]
+
+for n in nums:
+    if n > largest:
+        largest = n
+
+print(largest)
+#output:90
