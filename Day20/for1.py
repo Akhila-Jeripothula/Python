57d5820a8b1ddd310552f857fb4d3ab8b4eddb2b
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-01-19 18:31:54 +0530

    forrange

---
+++ Day20/for1.py
@@ -0,0 +1,20 @@
+#for loop
+
+#for loop is used to repeat a block of code multiple times
+
+#1
+for i in [1,2,3]:
+ print(i)
+
+#2  (range with for loop)
+for i in range(10):
+ print(i)
+
+#3.  (range: start,stop)
+for i in range(1,8):
+ print(i)
+
+#4.  (range: start,stop,step)
+for i in range(1,9,3):
+ print(i)
+ 
