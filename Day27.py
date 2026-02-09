da372aad8d4135948b0c1399b81fff08f37f0b3b
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-02-09 10:00:00 +0530

    push

--- Day27.py
+++ Day27.py
@@ -30,3 +30,27 @@ sub(10, 5)
 def sub(a,b):
     print(a-b)
 sub(b=5,a=10)
+
+#3.default parameter
+#if a value is not used then a default one is passed
+def greet(name="akhi"):
+    print("Hello",name)
+greet()
+greet("Akhila")
+#output
+# Hello akhi
+# Hello Akhila
+
+#4.Variable Length parameters(*args)
+#used when number of arguments is unknown
+def add(*num):
+    print(sum(num))
+add(1,2,3,4)
+#OUTPUT=10
+
+#5. Keyword variable length parameters
+#used when arguments are passed as key value pairs
+def info(**data):
+    print(data)
+info(name="Akhila", age=21, course="Python")
+#output:{'name': 'Akhila', 'age': 21, 'course': 'Python'}
