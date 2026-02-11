3aa64ec68cd130a72090a72cde6585c1a80d5171
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-02-11 19:38:32 +0530

    commit

--- Day28.py
+++ Day28.py
@@ -5,3 +5,17 @@ def add(a, b):
 
 result = add(5, 3)
 print(result)
+
+
+#scope
+
+# Where a variable can be accessed in the program.
+#1.Local scope
+
+  # A variable created inside a function.
+  # It can be used only inside that function.
+
+def myfunc():
+    x=30
+    print(x)
+myfunc()
