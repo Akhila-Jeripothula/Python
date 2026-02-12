9001e7f319cb95a29cd7c4919dc48a99c6ddb62d
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-02-12 18:42:37 +0530

    recursion

--- Day29.py
+++ Day29.py
@@ -1,5 +1,25 @@
-#PYTHON FUNCTIONS-DECORATORS
+#PYTHON FUNCTIONS-RECURSION
 
-#Decorators:
-# Decorators let you add extra behavior to a function, without changing the function's code.
-# A decorator is a function that takes another function as input and returns a new function.
+#A recursive function is a function that solves a problem by calling itself.
+
+#1. factorial
+def factorial(n):
+    if n == 1:           # Base case
+        return 1
+    else:
+        return n * factorial(n - 1)   # Recursive case
+print(factorial(5))
+
+#output:120
+
+#2.
+def print_numbers(n):
+    if n == 0:
+        return
+    print(n)
+    print_numbers(n - 1)
+
+print_numbers(5)
+
+
+
