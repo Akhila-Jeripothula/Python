ca266eb0881bda581caccb554954a55b3e6e1582
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-02-17 19:23:53 +0530

    push

--- Day32.py
+++ Day32.py
@@ -3,12 +3,28 @@
 #1.Two Sum problem
 
 #brute force:
-def twosum(nums,target):
+# def twosum(nums,target):
+#     for i in range(len(nums)):
+#         for j in range(i+1,len(nums)):
+#             if nums[i]+nums[j]==target:
+#                 return [i,j]
+# nums=[1,2,3]
+# target=5
+# result=twosum(nums,target)
+# print(result)
+
+#dictionary
+def twoSum(nums, target):
+    seen = {}   # dictionary to store number and its index
+    
     for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-            if nums[i]+nums[j]==target:
-                return [i,j]
-nums=[1,2,3]
-target=5
-result=twosum(nums,target)
+        needed = target - nums[i]
+        
+        if needed in seen:
+            return [seen[needed], i]
+        
+        seen[nums[i]] = i
+nums=[2,7,11,15]
+target=9
+result=twoSum(nums,target)
 print(result)
