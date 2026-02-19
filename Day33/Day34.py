4d688edba2d65d414f49c4f84797d37d135e6022
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-02-19 19:57:29 +0530

    commit

--- Day33/Day34.py
+++ Day33/Day34.py
@@ -1,2 +1,17 @@
 #FUNCTIONS
 
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+@changecase
+def myfunction():
+  return "Hello"
+
+@changecase
+def otherfunction():
+  return "I am Akhila!"
+
+print(myfunction())
+print(otherfunction())
