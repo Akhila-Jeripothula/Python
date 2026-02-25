b6a91180b7ca556fb427e10e731a1517e2be43e6
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-02-25 16:06:19 +0530

    rev

---
+++ Day 36.py
@@ -0,0 +1,19 @@
+#global and local variable
+x="Akhila"
+def mufunc():
+    x="Akshaya"
+    print("My name is",x)
+mufunc()
+print("My name is",x)
+
+#data types
+#type conversion
+x=5.5
+print(complex(x))
+
+#strings
+strr="Akhila is a good girl"
+print("good" not in strr)
+
+str="hello"
+print(str[::-1])
