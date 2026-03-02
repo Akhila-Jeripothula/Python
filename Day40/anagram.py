25dee0f09dae47a66d162a167cbf1d6041697a8d
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-03-02 19:45:56 +0530

    40

---
+++ Day40/anagram.py
@@ -0,0 +1,5 @@
+# Two strings are anagrams if they contain the same characters with same frequency.
+def is_anagram(s1, s2):
+    return sorted(s1) == sorted(s2)
+
+print(is_anagram("listen", "silent"))  # True
