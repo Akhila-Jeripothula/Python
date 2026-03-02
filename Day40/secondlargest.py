a281173a9929d5393c327199113aa634e7b737c9
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-03-02 19:48:09 +0530

    40

---
+++ Day40/secondlargest.py
@@ -0,0 +1,16 @@
+def second_largest(nums):
+    first = second = float('-inf')
+    
+    for num in nums:
+        if num > first:
+            second = first
+            first = num
+        elif num > second and num != first:
+            second = num
+            
+    return second
+
+print(second_largest([10, 20, 4, 45, 99]))
+
+#output:
+#45
