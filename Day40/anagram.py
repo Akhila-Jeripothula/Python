9479cca1c8f2a57bceb12d8dc3f06f30b75f956c
Akhila Jeripothula <akhilajeripothula075@gmail.com>
2026-03-09 20:15:19 +0530

    Update anagram.py

--- Day40/anagram.py
+++ Day40/anagram.py
@@ -1,4 +1,4 @@
-# Two strings are anagrams if they contain the same characters with same frequency.
+ # Two strings are anagrams if they contain the same characters with same frequency.
 def is_anagram(s1, s2):
     return sorted(s1) == sorted(s2)
 
