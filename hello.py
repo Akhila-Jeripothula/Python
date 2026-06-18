cef171faa713fa942e8c070e136f511c9f7264c7
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-06-18 22:51:54 +0530

    push

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+print ("hello andharu bagunnaara")
+
+name="Akhila Jeripothula"
+age=20
+description="She is very confused in her life"
+gpa=8.1
+print(name,age,description,gpa)
