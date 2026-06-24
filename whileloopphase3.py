6f6da7e132a39abd37b13fa2f10792c47206d729
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-06-24 20:45:06 +0530

    phase3

--- whileloopphase3.py
+++ whileloopphase3.py
@@ -6,16 +6,56 @@
 
 
 #1.ARMSTRONG NUMBER:
-n=407
-original=n
-total=0
-while n>0:
-    digit=n%10
-    n=n//10
-    total=total+digit**3
-if(total==original):
-    print("Armstrong number")
-else:
-    print("Not an Armstrong Number")
+# n=407
+# original=n
+# total=0
+# while n>0:
+#     digit=n%10
+#     n=n//10
+#     total=total+digit**3
+# if(total==original):
+#     print("Armstrong number")
+# else:
+#     print("Not an Armstrong Number")
 
+
+#2.FIBONACCI:
+# n=7
+# a=0
+# b=1
+# count=1
+# while count<=n:
+#     print(a)
+#     c=a+b
+#     a=b
+#     b=c
+#     count+=1
+
+
+# n=5
+# a=0
+# b=1
+# count=1
+# while count<=n:
+#     print(a)
+#     c=a+b
+#     a=b
+#     b=c
+#     count+=1
+
+
+#print the first 10 fibonacci sequence:
+# n=10
+# a=0
+# b=1
+# count=1
+# while count<=n:
+#     print(a)
+#     c=a+b
+#     a=b
+#     b=c
+#     count+=1
+
+
+#print fibonacci number which are less than 50
 
