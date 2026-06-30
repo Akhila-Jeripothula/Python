9c79b559d5a028bd67f66d35bf015fc2aeafe551
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-07-01 00:09:49 +0530

    push

--- whileloopphase4.py
+++ whileloopphase4.py
@@ -301,11 +301,173 @@ i=1
 
 #  OR
 
-i=1
-while i<=5:
-    j=2
-    while j<=i+1:
-        print(j,end="")
-        j+=1
-    print()
-    i+=1
+# i=1
+# while i<=5:
+#     j=2
+#     while j<=i+1:
+#         print(j,end="")
+#         j+=1
+#     print()
+#     i+=1
+
+
+
+#6.
+
+# 5
+# 54
+# 543
+# 5432
+# 54321
+
+# i=1
+# while i<=5:
+#     j=5
+#     while j>=6-i:
+#         print(j,end="")
+#         j-=1
+#     print()
+#     i+=1
+
+
+
+
+#7.
+
+# 9
+# 98
+# 987
+# 9876
+# 98765
+
+# i=1
+# while i<=5:
+#     j=5
+#     while j>=6-i:
+#         print(j+4,end="")
+#         j-=1
+#     print()
+#     i+=1
+
+
+#  OR
+
+# i=1
+# while i<=5:
+#     j=9
+#     while j>=10-i:
+#         print(j,end="")
+#         j-=1
+#     print()
+#     i+=1
+
+
+
+#8.
+
+# 8
+# 87
+# 876
+# 8765
+# 87654
+
+
+# i=1
+# while i<=5:
+#     j=8
+#     while j>=9-i:
+#         print(j,end="")
+#         j-=1
+#     print()
+#     i+=1
+
+
+#9.
+# 3
+# 34
+# 345
+# 3456
+# 34567
+
+# i=1
+# while i<=5:
+#     j=3
+#     while j<=2+i:
+#         print(j,end="")
+#         j+=1
+#     print()
+#     i+=1
+
+
+
+#10.
+# 7
+# 77
+# 777
+# 7777
+# 77777
+
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print("7",end="")
+#         j+=1
+#     print()
+#     i+=1
+
+
+
+#11.
+# 12345
+# 1234
+# 123
+# 12
+# 1
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=6-i:
+#         print(j,end="")
+#         j+=1
+#     print()
+#     i+=1
+
+
+#12.
+
+# *****
+# ****
+# ***
+# **
+# *
+
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=6-i:
+#         print("*",end="")
+#         j+=1
+#     print()
+#     i+=1
+
+
+#13.
+# 1
+# 21
+# 321
+# 4321
+# 54321
+
+# i=1
+# while i<=5:
+#     j=i
+#     while j>=1:
+#         print(j,end="")
+#         j-=1
+#     print()
+#     i+=1
+
