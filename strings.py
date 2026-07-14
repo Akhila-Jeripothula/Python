ad7c2856cea268588c73ad734b9ab4f76d0b3932
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-07-14 14:07:45 +0530

    push

--- strings.py
+++ strings.py
@@ -51,5 +51,56 @@
 
 
 #7.reverse string:
-s="python"
-print(s[::-1])
+# s="python"
+# print(s[::-1])
+
+
+#8. count frequency of charecters:
+
+# s="banana"
+# target="a"
+# count=0
+# for ch in s:
+#     if ch==target:
+#         count+=1
+# print(count)
+
+
+
+#9. check whether charecter exists or not
+# s="python"
+# found=False
+# for ch in s:
+#     if ch=="o":
+#         found=True
+# if(found==True):
+#     print("charecter exists")
+# else:
+#     print("charecter not exists")
+
+
+
+#10.count special charecters:
+# s="abc@#12$"
+# count=0
+# for ch in s:
+#     if ch not in ch.isalpha() and ch.isdigit() and ch==" ":
+#         count+=1
+# print(count)
+
+#11.find the first uppercase:
+# s="pythonIsFun"
+# for ch in s:
+#     if ch.isupper():
+#         break
+# print(ch)
+
+#12.Find the first digit:
+# s="abc123xyz"
+# for ch in s:
+#     if ch.isdigit():
+#         break
+# print(ch)
+
+
+
